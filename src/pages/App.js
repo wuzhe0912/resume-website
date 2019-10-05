813be833469b7efc3d562f6781abafcaa279879a
zhethon <kgb00128@gmail.com>
2019-10-05 08:21:52 +0800

    建立組件結構、樣式

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,18 +1,37 @@
 import React from 'react';
 // import logo from './logo.svg';
 import styles from './App.module.scss';
+import { Link, Route } from 'react-router-dom';
+import HomePage from './HomePage'
+import WorkPage from './WorkPage'
+import WorkPageDetail from './WorkPageDetail'
 
 function App() {
   return (
     <div className={styles.App}>
       <header className={styles.header}>
-        header
+        <div className={styles.nav}>
+          <Link to="/">
+            <div className={styles.nav__title}>
+              Pitt Wu
+            </div>
+          </Link>
+          <div className={styles.nav__list}>
+            <Link to="/"><p>HOME</p></Link>
+            <Link to="/works"><p>WORKS</p></Link>
+          </div>
+        </div>
       </header>
       <section className={styles.content}>
-        content
+        <Route path="/" component={ HomePage } />
+        <Route path="/works" component={ WorkPage } />
+        <Route path="/works:id" component={ WorkPageDetail } />
       </section>
-      <footer>
-        footer
+      <footer className={styles.footer}>
+        <h3>
+          <b>© 2019 Pitt Wu</b>
+        </h3>
+        <p>Handmade with</p>
       </footer>
     </div>
   );
