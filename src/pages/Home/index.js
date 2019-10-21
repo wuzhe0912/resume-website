cc904873effc41e4b5aec1524284a0d9a899aa6e
Pitt Wu <kgb00128@gmail.com>
2019-10-21 10:26:58 +0800

    修改首頁

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
-import { styles } from './style.module.scss'
+import style from './style.module.scss'
 
 export default () => {
-  return <div className={styles.wrap}>Home</div>
+  return <div className={ style.wrap }>
+    {/* 個人簡介 */}
+    <section className={ style.brief }>
+      <img src={ require("../../assets/images/avatar.jpg") } alt=""/>
+      <h1>Pitt Wu</h1>
+      <ul>
+        <li>Frontend Developer</li>
+      </ul>
+      <p>
+        test content
+      </p>
+    </section>
+    {/* Demo 簡述 */}
+    <section className={style.demo}>
+      <div className={style.title}>
+        <h2>A section of demo</h2>
+        <p>You can find more in the work overview.</p>
+      </div>
+    </section>
+  </div>
 }
